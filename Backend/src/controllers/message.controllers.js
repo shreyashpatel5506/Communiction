39e2b031f53690c5b508f3428e662c2eabbc1a49
Shreyash Patel <160259841+shreyashpatel5506@users.noreply.github.com>
2025-06-03 16:46:52 +0530

    Add message handling functions and update routes for message operations

--- Backend/src/controllers/message.controllers.js
+++ Backend/src/controllers/message.controllers.js
@@ -1,6 +1,7 @@
 import Follower from "../models/follwer.user.model.js";
 import Message from "../models/message.model.js";
 import user from "../models/user.model.js";
+import claudinary from "../lib/cloudinary.js";
 
 //for the followers user fetch
 export const fetchfollowersuser= async(req , res) =>{
@@ -21,7 +22,16 @@ export const fetchfollowersuser= async(req , res) =>{
 export const fetchalluser = async(req , res) =>{
   try{
     const userId = req.user._id;
-    const alluser = await user.find({_id:{$ne:userId}});
+    const searchQuery = req.query;
+    const query = { _id: { $ne: userId } };
+
+    if(searchQuery){
+      query.$or = [
+        {name:{$regex:search,$options:'i'}},
+        {username:{$regex:searchQuery,$options:'i'}},
+      ];
+    }
+    const alluser = await user.find(query);
     res.status(200).json({
       message:"All users fetched",
       success:true,
@@ -29,5 +39,52 @@ export const fetchalluser = async(req , res) =>{
     });
   }catch(error){
     res.status(500).json({message:error.message});
+  }
+}
+
+export const sendmessage = async ( req ,res)=>{
+  try {
+    const {senderId} = req.user._id;
+    const {reciverId} = req.params;
+    const {text} = req.body;
+    const {image} = req.body;
+    let imageurl;
+    if(image){
+      const uploadresponse = await cloudinary.uploader.upload(image)
+      imageurl = uploadresponse.secure_url();
+    }
+    const message = new Message({
+      senderId,
+      reciverId,
+      text,
+      image:imageurl,
+    });
+    await message.save();
+    res.status(200).json({
+      message:"Message sent",
+      success:true,
+    });
+  } catch (error) {
+    return res.status(500).send("Error" + error.message);
+  }
+}
+
+export const getmessage =  async (req, res) => {
+  try {
+    const myid= req.user._id;
+    const {id : chatId} = req.params;
+    const messages = await Message.find({
+      $or: [
+        { senderId: myid, receiverId: chatId },
+        { senderId: chatId, receiverId: myid },
+      ],
+    });
+    res.status(200).json({
+      message:"Messages fetched",
+      success:true,
+      messages,
+    });
+  } catch (error) {
+    return res.status(500).send("Error" + error.message);
   }
 }
--- Backend/src/routes/message.routes.js
+++ Backend/src/routes/message.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { protectRoute } from '../middlewears/auth.middlewear.js';
+import { fetchfollowersuser, fetchalluser, sendmessage, getmessage } from '../controllers/message.controllers.js';
 
 const router = express.Router();
 
-// router.post('/follwers',protectRoute,fetchfollowersuser);//for messages
-// router.post('/alluser',protectRoute,fetchalluser);//for started follow any user 
-// router.post('/send-message',protectRoute,sendmessage);
-// router.post('/get-message',protectRoute,getmessage);
+router.get('/follwers',protectRoute,fetchfollowersuser);//for messages
+router.post('/alluser',protectRoute,fetchalluser);//for started follow any user 
+router.post('/send-message/:id',protectRoute,sendmessage);
+router.post('/get-message/:id',protectRoute,getmessage);
 
 export default router;
