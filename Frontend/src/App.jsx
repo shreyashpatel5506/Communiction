68a759164c97d2e96bf35c2f6473603fa67dcbd5
Shreyash Patel <160259841+shreyashpatel5506@users.noreply.github.com>
2025-06-04 11:50:36 +0530

    feat: implement routing and add pages for homepage, login, signup, profile, and settings

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Navbar from './components/Navbar.jsx'; 
+import { Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage.jsx';
+import Loginpage from './pages/Loginpage.jsx';
+import Signuppage from './pages/Signuppage.jsx';
+import Profilepage from './pages/Profilepage.jsx';
+import Settingpage from './pages/Settingpage.jsx';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+    <Navbar />
+
+    <Routes>
+          <Route path='/' element={<Homepage/>} />
+          <Route path='/login' element={<Loginpage/>} />
+          <Route path='/signup' element={<Signuppage/>} />
+          <Route path='/profile' element={<Profilepage/>}/>
+          <Route path='/settings' element={<Settingpage />} />
+       </Routes>
+       </div>
   )
 }
 
