19f42d0a709ea21a90a43eb5ee70d620f21903ed
Shreyash Patel <you@example.com>
2025-06-20 22:28:32 +0530

    fix: update Homepage component to use h-max for responsive height adjustment

--- Frontend/src/pages/Homepage.jsx
+++ Frontend/src/pages/Homepage.jsx
@@ -8,7 +8,7 @@ const Homepage = () => {
   const { selectedUser } = useChatStore();
 
   return (
-    <div className="h-screen bg-base-200">
+    <div className="h-max bg-base-200">
       <div className="flex item-center justify-center pt-20 px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh - 8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
