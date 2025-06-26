5330749cf9d99fa0bf061d31a7b00bf9d479661d
Shreyash Patel <you@example.com>
2025-06-26 19:38:49 +0530

    fix: correct import path for MessageInput component in ChatContainer

--- Frontend/src/components/ChatContainer.jsx
+++ Frontend/src/components/ChatContainer.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { useChatStore } from '../StoreValues/useChat.Store';
 import ChatHeader from './ChatHeader';
-import MessageInput from './MessageInput,jsx';
+import MessageInput from './MessageInput.jsx';
 import MessageseSkelton from './skelton/MessageseSkelton.jsx';
 import { useAuth } from '../StoreValues/useAuth.Store.js';
 import avatarFallback from "../assets/avatar-default-symbolic.svg" // Adjust the path as necessarys
